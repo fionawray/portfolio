a6700eb234045085e579b52a5813147f3a24805e
Fiona Wray <fionawray@fionas-mbp.home>
2019-06-13 10:06:51 +0200

    portfolio

---
+++ script.js
@@ -0,0 +1,72 @@
+const words = ["Multimediedesigner.", "KEA.", "HTML."];
+let i = 0;
+let timer;
+
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+    document.querySelector("#mere").addEventListener("click", readMore);
+}
+
+function toggleMenu() {
+    console.log("toggleMenu");
+    document.querySelector("#menu").classList.toggle("hidden");
+
+    let erSkjult =
+        document.querySelector("#menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent = "☰";
+    } else {
+        document.querySelector("#menuknap").textContent = "X";
+    }
+}
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function readMore() {
+    console.log("readMore");
+    document.querySelector("#box").classList.toggle("stor");
+    document.querySelector("#box").classList.toggle("lille");
+}
+
+function typingEffect() {
+    let word = words[i].split("");
+    let loopTyping = function () {
+        if (word.length > 0) {
+            document.getElementById('word').innerHTML += word.shift();
+        } else {
+            deletingEffect();
+            return false;
+        };
+        timer = setTimeout(loopTyping, 200);
+    };
+    loopTyping();
+};
+
+function deletingEffect() {
+    let word = words[i].split("");
+    let loopDeleting = function () {
+        if (word.length > 0) {
+            word.pop();
+            document.getElementById('word').innerHTML = word.join("");
+        } else {
+            if (words.length > (i + 1)) {
+                i++;
+            } else {
+                i = 0;
+            };
+            typingEffect();
+            return false;
+        };
+        timer = setTimeout(loopDeleting, 100);
+    };
+    loopDeleting();
+};
+
+typingEffect();
